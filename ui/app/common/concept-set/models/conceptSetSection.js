ee73adb6020e258c8833fd74d6d22b0b95d05ef5
Bailly RURANGIRWA <rubailly@gmail.com>
2019-04-03 13:20:13 +0200

    COM-167: Formatting code after CI build failure -error  Mixed spaces and tabs     no-mixed-spaces-and-tabs

--- ui/app/common/concept-set/models/conceptSetSection.js
+++ ui/app/common/concept-set/models/conceptSetSection.js
@@ -23,7 +23,7 @@ Bahmni.ConceptSet.ConceptSetSection = function (extensions, user, config, observ
         self.allowAddMore = config.allowAddMore;
         self.id = "concept-set-" + conceptSet.uuid;
         if (self.uuid === "1fb2dd86-53b5-4815-9c64-edc081b908d9") {
-        	self.collapseInnerSections = {value: true};
+            self.collapseInnerSections = {value: true};
         }
     };
 
