01c3e62a921a2041cf74c8b8b84dc6dbbd2f1a68
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2020-01-20 12:43:26 +0200

    COM-597 Fix breaking tests Set the language value only when the variable currentUser.userProperties is defined in $rootScope.

--- ui/app/common/patient-search/controllers/patientsListController.js
+++ ui/app/common/patient-search/controllers/patientsListController.js
@@ -41,7 +41,7 @@ angular.module('bahmni.common.patientSearch')
                 var params = { q: searchType.handler, v: "full",
                     location_uuid: $bahmniCookieStore.get(Bahmni.Common.Constants.locationCookieName).uuid,
                     provider_uuid: $rootScope.currentProvider.uuid,
-                    language: $rootScope.currentUser.userProperties.defaultLocale };
+                    language: ($rootScope.currentUser && $rootScope.currentUser.userProperties)?$rootScope.currentUser.userProperties.defaultLocale:null };
                 if (searchType.additionalParams) {
                     params["additionalParams"] = searchType.additionalParams;
                 }
